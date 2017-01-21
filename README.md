e458dcc065b309bf3bbd6399648979a4544e9a6a
Troy Forster <troy.forster@gmail.com>
2017-01-20 21:00:18 -0500

    Initial creation.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Bot POC
+
+A quick proof of concept for a Microsoft Teams 1:1 chat Bot
+
+# Resources
+
+https://docs.botframework.com
+https://blogs.msdn.microsoft.com/sarahsays/2015/08/31/building-your-first-node-js-app-and-publishing-to-azure/
+https://msdn.microsoft.com/en-us/microsoft-teams/bots
+http://www.funny2.com/ralph.htm
+
+# Built With
+
+* Windows 10 Home Anniversary edition
+* Windows Subsystem for Linux (WSL)
+* Visual Studio Code 
+* NodeJS 7.2.0
+* Microsoft Azure 
+* Cabernet Sauvignon
+
---
+++ app.js
@@ -0,0 +1,21 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+var quotes = require('./data.json');
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+  console.log('%s listening to %s', server.name, server.url);
+});
+
+var connector = new builder.ChatConnector({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+bot.dialog('/', function (session) {
+  var quote = quotes[parseInt(Math.random() * (quotes.length - 1))];
+  session.send(quote);
+});
