8347d79b2f477f54f58a84120a4e00f986f35394
Troy Forster <troy.forster@gmail.com>
2017-01-23 19:35:31 -0500

    Updated botbuilder module to fix https://github.com/Microsoft/BotBuilder/issues/1955

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Bot POC
 
-A quick proof of concept for a Microsoft Teams 1:1 chat Bot
+A quick proof of concept for a Microsoft Teams 1:1 chat Bot.
+
+A trivial bot that regardless of what you say responds with a random quote from [Ralph Wiggum](https://en.wikipedia.org/wiki/Ralph_Wiggum)
 
 # Resources
 
-https://docs.botframework.com
-https://blogs.msdn.microsoft.com/sarahsays/2015/08/31/building-your-first-node-js-app-and-publishing-to-azure/
-https://msdn.microsoft.com/en-us/microsoft-teams/bots
-http://www.funny2.com/ralph.htm
+* https://docs.botframework.com
+* https://blogs.msdn.microsoft.com/sarahsays/2015/08/31/building-your-first-node-js-app-and-publishing-to-azure/
+* https://msdn.microsoft.com/en-us/microsoft-teams/bots
+* https://dev.botframework.com/bots/new
+* http://www.funny2.com/ralph.htm
+* https://github.com/Microsoft/BotBuilder/issues/1955
 
 # Built With
 
--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var restify = require('restify');
-var builder = require('botbuilder');
-var quotes = require('./data.json');
+'use strict'
 
-var server = restify.createServer();
+const restify = require('restify');
+const builder = require('botbuilder');
+const quotes = require('./data.json');
+
+let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
   console.log('%s listening to %s', server.name, server.url);
 });
 
-var connector = new builder.ChatConnector({
+let connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-var bot = new builder.UniversalBot(connector);
+let bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 bot.dialog('/', function (session) {
-  var quote = quotes[parseInt(Math.random() * (quotes.length - 1))];
+  let quote = quotes[parseInt(Math.random() * (quotes.length - 1))];
   session.send(quote);
 });
